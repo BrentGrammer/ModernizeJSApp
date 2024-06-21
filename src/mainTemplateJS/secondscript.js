36c52192bb11acd4a6cc74d4d2d2f85085a32885
Brent Marquez <brentonmarquez@gmail.com>
2024-06-20 19:12:39 -0600

    figuring out imports

--- src/mainTemplateJS/secondscript.js
+++ src/mainTemplateJS/secondscript.js
@@ -1,3 +1,7 @@
-someGlobal.run = function () {
+import * as stuff from './firstscript.js';
+
+console.log({stuff}); // this is an empty object - why can't I import contents from firstscript.js?
+
+stuff.someGlobal.run = function () {
   return "hello from someGlobal.run in second script.";
 };
