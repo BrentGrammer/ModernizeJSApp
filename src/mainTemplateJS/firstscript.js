d681b11670a88051feae3c18d657193b0912963a
Brent Marquez <brentonmarquez@gmail.com>
2024-06-21 16:41:02 -0600

    expor5t and import a function

--- src/mainTemplateJS/firstscript.js
+++ src/mainTemplateJS/firstscript.js
@@ -1,3 +1,7 @@
-console.log('Hello from firstscript.js');
+console.log("Hello from firstscript.js");
 
 window.someGlobal = {}; // This is a global variable used by other scripts listed underneath it in script tags in the original app.
+
+export const afunction = function (someArg) {
+  return "This is what was passed into firstcripts afunction: " + someArg;
+};
--- src/mainTemplateJS/thirdscript.js
+++ src/mainTemplateJS/thirdscript.js
@@ -1,2 +1,7 @@
+import { afunction } from "./firstscript";
+// This comment should not appear in the build files.
 console.log("Do we have someGlobal? ", someGlobal.run());
 console.log("hello from thirdscript.js");
+
+console.log(afunction('I come from thirdscript.js'));
+
