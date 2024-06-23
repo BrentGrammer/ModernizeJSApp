6bff42783c24e41f3bc0f8cb1a85240a99909497
Brent Marquez <brentonmarquez@gmail.com>
2024-06-23 17:48:24 -0600

    add jQuery

--- src/firstscript.js
+++ src/firstscript.js
@@ -1,3 +1,13 @@
 console.log("Hello from firstscript.js");
 
 window.someGlobal = {};
+
+console.log("loading jquery?", $);
+$.when($.ready).then(function () {
+  console.log("JQuery recognized in firstscript.js. Document is ready.");
+});
+
+// appManager is a global set in a script tag in mainTemplate.cfm
+appManager.updateDisplay = function () {
+  console.log("I am updating the display - app manager.");
+};
